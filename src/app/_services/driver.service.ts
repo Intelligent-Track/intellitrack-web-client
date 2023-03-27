97b4e6d0e564b60437f295c51cd09ea1d9721e8c
Nicolas Ramos <nicolas-ramos@javeriana.edu.co>
2023-03-27 12:48:34 -0500

    Revert "Merge branch 'develop' of https://github.com/Intelligent-Track/intellitrack-web-client into develop"
    This reverts commit 002055e37c251db388e02b5a6dc92aae24957e65, reversing
    changes made to 5c2e98d47cf8752d66a63cdd634b4c293e60402b.

--- src/app/_services/driver.service.ts
+++ src/app/_services/driver.service.ts
@@ -4,7 +4,7 @@ import { Observable } from 'rxjs';
 import { DtoDriver } from '../dto/dto-driver';
 import { Driver } from '../model/driver';
 
-const DRIVER_API = 'http://localhost:8080/api/dri/';
+const DRIVER_API = 'http://localhost:8080/api/driver/';
 
 @Injectable({
   providedIn: 'root'
@@ -12,8 +12,8 @@ const DRIVER_API = 'http://localhost:8080/api/dri/';
 export class DriverService {
 
   private httpOptions = {
-    headers: new HttpHeaders({
-      "Content-Type": "application/json"
+    headers : new HttpHeaders({
+      "Content-Type" : "application/json"
     })
   }
   constructor(private http: HttpClient) { }
