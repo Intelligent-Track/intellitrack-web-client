87c0594994f7723583f0f6f14b2a38b3cf1a8ac7
nrb3098 <54920583+nrb3098@users.noreply.github.com>
2023-04-15 15:02:14 -0500

    parcial de end points

--- src/app/vehicle-list-crud/vehicle-list-crud.component.ts
+++ src/app/vehicle-list-crud/vehicle-list-crud.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Vehicle } from '../model/vehicle';
+import { VehicleService } from '../_services/vehicle.service';
 @Component({
   selector: 'app-vehicle-list-crud',
   templateUrl: './vehicle-list-crud.component.html',
@@ -7,9 +8,36 @@ import { Component, OnInit } from '@angular/core';
 })
 export class VehicleListCRUDComponent implements OnInit {
 
-  constructor() { }
+  constructor(private vehicleService: VehicleService) { }
+
+  vehicleArray: Vehicle[] = [];
+
+  selectedVehicle: Vehicle = new Vehicle(0, "","","","",0,0);
 
   ngOnInit(): void {
+    this.vehicleService.listAllVehicles().subscribe(vehicle => {this.vehicleArray = vehicle});
   }
 
+
+  editPanther(vehicle:  Vehicle){
+    this.selectedVehicle = vehicle;
+
+  }
+
+  agregar(){
+    if(this.selectedVehicle.id === 0){
+      this.selectedVehicle.id = this.vehicleArray.length + 1
+      this.vehicleArray.push(this.selectedVehicle);
+    }
+    this.selectedVehicle = new Vehicle(0, "","","","",0,0);
+  }
+
+  deletePanther(id: number){
+    if(confirm("¿Esta seguro que desea eliminar ese traje?")){
+      this.vehicleArray = this.vehicleArray.filter(x => x != this.selectedVehicle);
+      this.selectedVehicle = new Vehicle(0, "","","","",0,0);
+      console.log(id);
+    }
+
+  }
 }
